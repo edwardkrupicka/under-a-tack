ede09fc31a5b56c7e1dadc86141f9c25adbd1aa3
Delilah Necrason <delilahnecrason@gmail.com>
2022-01-06 18:53:40 -0500

    Cart iteration 1

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,38 @@
+import React from 'react';
 import './Cart.scss';
 
-const Cart = () => {
+const Cart = ({items}) => {
+
+
+  const cartItems = items.map((product) => {
+        return (
+          <div className="single-item">
+            <img src={product.url} alt={`${product.title} by ${product.artist}`}></img>
+            <p></p>
+            <p>{product.price}</p>
+          </div>
+        )
+    })
 	
 	return (
-		<h1>CART</h1>
+		<div className="cart">
+      <h2 className="cart-header">your cart</h2>
+      <div className="cart-bar"></div>
+      <div className="cart-columns">
+        <p className="cart-text">Item</p>
+        <p className="cart-text">Quantity</p>
+        <p className="cart-text">Price</p>
+      </div>
+      <section className="cart-items">
+        {cartItems}
+      </section>
+      <section className="cart-finances">
+        <p className="cart-text">Subtotal ${}</p>
+        <p className="cart-text">Tax ${}</p>
+        <p className="cart-text">Shipping Estimate ${}</p>
+        <h3 className="cart-total">Total ${}</h3>
+      </section>
+    </div>
 	)
 }
 
