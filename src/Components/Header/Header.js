f2372e0752053d0aacd94d0458ab08f8be89b08d
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-01-07 09:27:46 -0700

    add links to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import { NavLink, Link } from "react-router-dom";
 import "./Header.scss"
+
 const Header = () => {
 
 
   return (
     <header className="header">
-      <h1>Under-A-Tack</h1>
+      <h1>under a tack!</h1>
       <div className="button-container">
-        <button>Home</button>
-        <button>Favorite</button>
+        <NavLink to='/' className='nav-link' >HOME</NavLink>
+        <NavLink to='/favorites' className='nav-link' >Favorite</NavLink>
         <button>Cart</button>
       </div>
     </header>
