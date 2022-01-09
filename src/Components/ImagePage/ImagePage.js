e39869348c02434b5e5aa5df7aea2e180b37b838
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-01-09 15:12:19 -0700

    add fav toggle

--- src/Components/ImagePage/ImagePage.js
+++ src/Components/ImagePage/ImagePage.js
@@ -3,7 +3,7 @@ import '../Card/Card'
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const ImagePage = ({ addToCart, favorites, setFavorites }) => {
+const ImagePage = ({ addToCart, handleFavoritesClick }) => {
   const [newData, setNewData] = useState([]);
 
   const locationId = useLocation().pathname.split(':')[1]
@@ -18,44 +18,9 @@ const ImagePage = ({ addToCart, favorites, setFavorites }) => {
     fetchData(`http://localhost:3001/api/v1/images/${locationId}`)
   }, [])
 
-  const addToFavorites = async () => {
-    // console.log(newData)
-    // await fetch('http://localhost:3001/api/v1/favorites', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(newData)
-    // })
-    //     .then(res => res.json())
-    //     .catch(err => console.log(err))
-
-    setFavorites([...favorites, newData])
-    console.log('add', favorites)
-  }
-
-  const deleteFromFavorites = () => {
-    const newArray = favorites.forEach((element, index) => {
-       if(element.id === newData.id){
-         favorites.slice(index - 1, index)
-       }
-    })
-    console.log('delete', favorites)
-  }
-
-  const handleFavoritesClick = () => {
-    let foundData = false;
-    favorites.forEach(element => {
-      if(element.id === newData.id){
-        foundData = true;
-        console.log(foundData)
-      }
-    })
-    if(foundData) {
-      deleteFromFavorites()
-    } else {
-      addToFavorites()
-    }
+  const handleClick = (newData) => {
+    handleFavoritesClick(newData)
+    setNewData(newData => ({ ...newData, favorited: !newData.favorited }));
   }
 
   return (
@@ -66,7 +31,7 @@ const ImagePage = ({ addToCart, favorites, setFavorites }) => {
       <p>Color: {newData.color}</p>
       <p>Type: {newData.type}</p>
       <button onClick={() => addToCart(locationId)}> add to cart</button>
-      <button onClick={() => handleFavoritesClick()}>FAVORITE</button>
+      <button className={newData.favorited ? 'favOn' : 'favOff'} onClick={() => handleClick(newData)}>FAVORITE</button>
     </div>
   )
 }
