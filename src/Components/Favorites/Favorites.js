a54a9db1dbea7f73153520e25eb4f3fa98f9140f
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-01-09 15:11:55 -0700

    add favoritesGrid

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -1,24 +1,14 @@
 import './Favorites.scss';
 import React, { useState, useEffect } from 'react';
 import Card from '../Card/Card'
-import Grid from '../Grid/Grid';
+import FavoritesGrid from '../FavoritesGrid/FavoritesGrid';
 
-const Favorites = () => {
-  const [favData, setFavData] = useState([])
-
-  useEffect(() => {
-    const fetchData = async (api) => {
-      const response = await fetch(api)
-      const responseJson = await response.json()
-      setFavData(responseJson)
-    }
-    fetchData(`http://localhost:3001/api/v1/favorites`)
-  }, [])
+const Favorites = ({ favorites, setFavorites, handleFavoritesClick }) => {
 
   return (
     <section className='favorites'>
       <h1>favorites</h1>
-      <Grid data={favData}/>
+      <FavoritesGrid favorites={favorites} setFavorites={setFavorites} handleFavoritesClick={handleFavoritesClick}/>
     </section>
   )
 }
