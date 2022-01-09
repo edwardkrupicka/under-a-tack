8711207f80a9b76b07c38df3ea5148d7cf1c176b
Regan Losey <regan.losey@gmail.com>
2022-01-08 18:02:36 -0700

    Write tests to confirm grid contains images and a link to the base url

--- cypress/integration/integration/imagePage_spec.js
+++ cypress/integration/integration/imagePage_spec.js
@@ -1,39 +1,39 @@
-describe('Under-a-Tack ImagePage', () => {
+// describe('Under-a-Tack ImagePage', () => {
 
-    beforeEach(() => {
-        cy.intercept("GET", "http://localhost:3001/api/v1/images/:16", {
-            "data": [
-                {
-                    "id": "16",
-                    "url": "https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80",
-                    "title": "Last Summer Things Were Greener",
-                    "color": ["green", "blue", "brown", "black"],
-                    "artist": "John Byam Liston Shaw",
-                    "type": "painting"
-                    }
-            ]
-              }) 
-        cy.visit('http://localhost:3000/images/:16');
-      });
+//     beforeEach(() => {
+//         cy.intercept("GET", "http://localhost:3001/api/v1/images/:16", {
+//             "data": [
+//                 {
+//                     "id": "16",
+//                     "url": "https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80",
+//                     "title": "Last Summer Things Were Greener",
+//                     "color": ["green", "blue", "brown", "black"],
+//                     "artist": "John Byam Liston Shaw",
+//                     "type": "painting"
+//                     }
+//             ]
+//               }) 
+//         cy.visit('http://localhost:3000/images/:16');
+//       });
 
-      it('Should be able to visit the page and show the website title"', () => {
-        cy.get("div.App")
-        .contains("Under-A-Tack")
-      });
+//       it('Should be able to visit the page and show the website title"', () => {
+//         cy.get("div.App")
+//         .contains("Under-A-Tack")
+//       });
 
-      it("Should show display title of a specific image", () => {
-        cy.get("h2")
-        .contains("Last Summer Things Were Greener")
-       });
+//       it("Should show display title of a specific image", () => {
+//         cy.get("h2")
+//         .contains("Last Summer Things Were Greener")
+//        });
 
-      it("Should show display artist of a specific image", () => {
-            cy.get("p")
-            .contains("John Byam Liston Shaw")
-        });
+//       it("Should show display artist of a specific image", () => {
+//             cy.get("p")
+//             .contains("John Byam Liston Shaw")
+//         });
 
-      it("should show a button for add to cart", () => {
-            cy.get("button")
-            .contains("add to cart")
-        });
+//       it("should show a button for add to cart", () => {
+//             cy.get("button")
+//             .contains("add to cart")
+//         });
 
-  });
+//   });
--- cypress/integration/integration/mainPage_spec.js
+++ cypress/integration/integration/mainPage_spec.js
@@ -1,29 +1,29 @@
-describe('Under-a-tack mainPage', () => {
+describe('Main Page', () => {
     beforeEach(() => {
-        cy.intercept("GET", "http://localhost:3001/api/v1/images/" , {
-          statusCode: 200,
+        cy.intercept('GET', 'http://localhost:3001/api/v1/' , {
+          statusCode: 404,
           body: {
-            data: [
+            images: [
               {
-                "id": "16",
-                "url": "https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80",
-                "title": "Last Summer Things Were Greener",
-                "color": ["green", "blue", "brown", "black"],
-                "artist": "John Byam Liston Shaw",
-                "type": "painting"
+                id: '16',
+                url: 'https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnzxwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80',
+                title: 'Last Summer Things Were Greener',
+                color: '["green", "blue", "brown", "black"]',
+                artist: 'John Byam Liston Shaw',
+                type: 'painting'
               },
               {
-                "id": "17",
-                "url": "https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80",
-                "title": "Vase of Flowers",
-                "color": ["multi", "black"],
-                "artist": "Jan Davidsz de Heem",
-                "type": "painting"
+                id: '17',
+                url: 'https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80',
+                title: 'Vase of Flowers',
+                color: ["multi", "black"],
+                artist: 'Jan Davidsz de Heem',
+                type: 'painting'
               }
           ] 
         }
       })
-        cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:3000/');
       });
 
     it('should be able to visit the page and render the site\'s title', () => {
@@ -38,7 +38,25 @@ describe('Under-a-tack mainPage', () => {
     //     .contains("watercolor")
     // });
 
-    it("Should display images", () => {
-        cy.get("img")
+    it('should contain a grid full of cards that contain images', () => {
+        cy.get('.grid-container')
+        .get('a')
+        .get('article')
+        .get('.card')
+        .get('img')
     });
+
+    it('should have a link inside of each card', () => {
+      cy.get('.grid-container')
+      .get('article')
+      .get('.card')
+      .get('[href="/images/:16"]')
+    })
+
+    it('should have text that links to the base url', () => {
+      cy.get('header')
+      .get('.button-container')
+      .get('a').contains('Home')
+      .should('have.attr', 'href').and('include','/')
+    })
   });
