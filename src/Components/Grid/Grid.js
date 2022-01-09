8ab2a2cd229141c64f7434ea45f88baff19a31fa
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-01-09 15:12:05 -0700

    Add handlefavorites click

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -2,18 +2,21 @@ import './Grid.scss';
 import Card from '../Card/Card'
 
 
-const Grid = ({ data }) => {
+const Grid = ({ data, handleFavoritesClick}) => {
+
   const cards = data.map((element) => {
-    return <Card
-      id={element.id}
-      key={element.id}
-      url={element.url}
-      title={element.title}
-      color={element.color}
-      artist={element.artist}
-      type={element.type}
-    />
-  })
+      return <Card
+        id={element.id}
+        key={element.id}
+        url={element.url}
+        title={element.title}
+        color={element.color}
+        artist={element.artist}
+        type={element.type}
+        favorited={element.favorited}
+        handleFavoritesClick={handleFavoritesClick}
+      />
+    })
 
   return (
     <section className="grid-container">
