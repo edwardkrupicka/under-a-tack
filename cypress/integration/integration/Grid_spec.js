3260271b468a449f6e5126e9d653aceec49716c0
Regan Losey <regan.losey@gmail.com>
2022-01-09 18:05:27 -0700

    Refactored Grid_spec and imagePage_spec tests to account for changes to header text.

--- cypress/integration/integration/Grid_spec.js
+++ cypress/integration/integration/Grid_spec.js
@@ -1,28 +1,6 @@
 describe('Grid', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/images/', {
-      statusCode: 200,
-      body: {
-        images: [
-          {
-            id: '16',
-            url: 'https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnzxwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80',
-            title: 'Last Summer Things Were Greener',
-            color: '["green", "blue", "brown", "black"]',
-            artist: 'John Byam Liston Shaw',
-            type: 'painting'
-          },
-          {
-            id: '17',
-            url: 'https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80',
-            title: 'Vase of Flowers',
-            color: ["multi", "black"],
-            artist: 'Jan Davidsz de Heem',
-            type: 'painting'
-          }
-        ]
-      }
-    })
+    cy.intercept('GET', 'http://localhost:3001/api/v1/images/', { statusCode: 200, fixture: 'images.json' })
     cy.visit('http://localhost:3000/');
   });
 
@@ -30,7 +8,7 @@ describe('Grid', () => {
     cy.url().should('eq', 'http://localhost:3000/')
     cy.get('header')
       .get('h1')
-      .contains('Under-A-Tack')
+      .contains('under a tack!')
   });
 
   it('should contain a grid full of cards that contain images', () => {
