a7d6685a97d76253bff51b0373e38c280603b447
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-01-10 09:28:13 -0700

    Add nav icons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.scss"
 
 const Header = () => {
@@ -6,11 +6,11 @@ const Header = () => {
 
   return (
     <header className="header">
-      <h1>under a tack!</h1>
+      <Link to='/' className='title-link' ><h1 className='title' >under a tack!</h1></Link>
       <div className="button-container">
-        <NavLink to='/' className='nav-link' >Home</NavLink>
-        <NavLink to='/favorites' className='nav-link' >Favorite</NavLink>
-        <NavLink to='/cart' >Cart</NavLink>
+        <NavLink to='/' className='nav-link' ><img className='home-icon' src='https://www.svgrepo.com/show/334004/home.svg' alt='cart.svg' /></NavLink>
+        <NavLink to='/favorites' className='nav-link' ><img className='fav-icon' src='https://www.svgrepo.com/show/333996/heart.svg' alt='cart.svg' /></NavLink>
+        <NavLink to='/cart' className='nav-link' ><img className='cart-icon' src='https://www.svgrepo.com/show/333784/cart-alt.svg' alt='cart.svg' /></NavLink>
       </div>
     </header>
   )
