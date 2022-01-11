e7878ddb2525061e663be42432c783fc532cee85
Tanya Zhuge <tangnazhuge@gmail.com>
2022-01-11 06:46:29 -0800

    Add image fixture and refactor intercept with fixture

--- cypress/integration/integration/imagePage_spec.js
+++ cypress/integration/integration/imagePage_spec.js
@@ -1,47 +1,11 @@
 describe('imagePage', () => {
   beforeEach(() => {
-    cy.intercept('http://localhost:3001/api/v1/images' , {
-      statusCode: 200,
-      body: {
-        images: [
-          {
-            id: 16,
-            url: 'https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnzxwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80',
-            title: 'Last Summer Things Were Greener',
-            color: '["green", "blue", "brown", "black"]',
-            artist: 'John Byam Liston Shaw',
-            type: 'painting'
-          },
-          {
-            id: 17,
-            url: 'https://images.unsplash.com/photo-1579783900882-c0d3dad7b119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=816&q=80',
-            title: 'Vase of Flowers',
-            color: ["multi", "black"],
-            artist: 'Jan Davidsz de Heem',
-            type: 'painting'
-          }
-      ] 
-    }
-  })
-    cy.intercept("http://localhost:3001/api/v1/images/:16", {
-      "data": [
-        {
-          id: 16,
-          url: "https://images.unsplash.com/photo-1577081320692-6eff449819c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=896&q=80",
-          title: "Last Summer Things Were Greener",
-          color: ["green", "blue", "brown", "black"],
-          artist: "John Byam Liston Shaw",
-          type: "painting"
-        }
-      ]
-    })
-    cy.visit('http://localhost:3000/images/:16');
+    cy.intercept('GET', 'http://localhost:3001/api/v1/images/16', { fixture: 'image' })
+    .intercept('GET', 'http://localhost:3001/api/v1/images', { fixture: 'images' })
+    .intercept('GET', 'http://localhost:3001/api/v1/favorites', { fixture: 'favorites' })
+    .visit('http://localhost:3000/images/:16')
   });
 
-  it('should visit the url that corresponds to clicked image\'s id', () => {
-    cy.url().should('eq', 'http://localhost:3000/images/:16')
-    
-  });
 
   it('should still render the header after routing', () => {
     cy.get('header')
